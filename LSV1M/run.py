becb7676f150b6370a1f85e6ecc369c19ae93ee0
RCagnol <racagnol@gmail.com>
2024-03-20 15:15:12 +0100

    README update + clean up

--- LSV1M/run.py
+++ LSV1M/run.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-This is implementation of model of self-sustained activitity in balanced networks from: 
-Vogels, T. P., & Abbott, L. F. (2005). 
-Signal propagation and logic gating in networks of integrate-and-fire neurons. 
-The Journal of neuroscience : the official journal of the Society for Neuroscience, 25(46), 10786–95. 
+This is implementation of model of corresponding to the pre-print `Large scale model of cat primary visual cortex`
+Antolík, J., Cagnol, R., Rózsa, T., Monier, C., Frégnac, Y., & Davison, A. P. (2018).
+https://www.biorxiv.org/content/10.1101/416156v5.abstract
 """
 import matplotlib
 matplotlib.use('Agg')
@@ -59,6 +58,6 @@ else:
     data_store = PickledDataStore(load=True, parameters=ParameterSet(
         {'root_directory': 'SelfSustainedPushPull_test____', 'store_stimuli': False}), replace=True)
 
-if mpi_comm.rank == 0:
+if mpi_comm.rank ==0 and not data_store.block.annotations["simulation_log"]["explosion_detected"]:
     print("Starting visualization")
     perform_analysis_and_visualization(data_store)
