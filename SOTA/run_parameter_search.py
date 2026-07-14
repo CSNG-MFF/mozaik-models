bf087f3d6d3d636e2123a9454e8fc71a7d72c046
antolikjan <antolikjan@gmail.com>
2026-07-14 21:01:53 +0200

    Replace hardcoded path with environment variable

--- SOTA/run_parameter_search.py
+++ SOTA/run_parameter_search.py
@@ -6,6 +6,6 @@ import time
 
 
 if True:
-    CombinationParameterSearch(SlurmSequentialBackend(num_threads=1, num_mpi=16,slurm_options=['--hint=nomultithread'],path_to_mozaik_env='$HOME/virt_env/mozaik-lognorm/bin/activate'), {
+    CombinationParameterSearch(SlurmSequentialBackend(num_threads=1, num_mpi=16,slurm_options=['--hint=nomultithread'],path_to_mozaik_env=PATH_TO_ENV), {
       'trial' : [1],
     }).run_parameter_search()
